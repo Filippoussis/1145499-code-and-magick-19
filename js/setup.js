85a13a3dce3f7be62decfd5b4e03fe3e1eda2adf
Filippoussis <filippoussis@iMacAndhilippos.planet-a.ru>
2020-02-09 12:52:42 +0500

    Выполнил 4 задание Учебного проекта: Одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -1,11 +1,16 @@
 'use strict';
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
 var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var setupUserName = setup.querySelector('.setup-user-name');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var MAX_WIZARDS = 4;
 
@@ -31,8 +36,8 @@ for (var i = 0; i < MAX_WIZARDS; i++) {
   // получение уникальных значений методом "швейцарского" ножа:)
   WIZARD_NAMES.splice(randomIndexFullName, 1);
   WIZARD_SURNAMES.splice(randomIndexFullName, 1);
-  COAT_COLORS.splice(randomIndexCoatColor, 1);
-  EYES_COLORS.splice(randomIndexEyesColor, 1);
+  // COAT_COLORS.splice(randomIndexCoatColor, 1);
+  // EYES_COLORS.splice(randomIndexEyesColor, 1);
 }
 
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -54,3 +59,64 @@ for (var j = 0; j < wizards.length; j++) {
 similarListElement.appendChild(fragment);
 
 setup.querySelector('.setup-similar').classList.remove('hidden');
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY && setupUserName !== document.activeElement) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+});
+
+var setupPlayer = document.querySelector('.setup-player');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+var wizardCoat = setupPlayer.querySelector('.wizard-coat');
+var wizardEyes = setupPlayer.querySelector('.wizard-eyes');
+var inputCoatColor = setupPlayer.querySelector('input[name = "coat-color"]');
+var inputEyesColor = setupPlayer.querySelector('input[name = "eyes-color"]');
+var inputFireballColor = wizardFireball.querySelector('input[name = "fireball-color"]');
+
+
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = COAT_COLORS[randomInteger(0, COAT_COLORS.length)];
+  inputCoatColor.value = wizardCoat.style.fill;
+});
+
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = EYES_COLORS[randomInteger(0, EYES_COLORS.length)];
+  inputEyesColor.value = wizardEyes.style.fill;
+});
+
+wizardFireball.addEventListener('click', function () {
+  inputFireballColor.value = FIREBALL_COLORS[randomInteger(0, FIREBALL_COLORS.length)];
+  wizardFireball.style.backgroundColor = inputFireballColor.value;
+});
